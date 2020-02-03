cba52439cd30a0de17291d5c26afb7487914846f
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-03 11:40:52 +0300

    feat: стилизация кнопки сравнения

--- components/compare/btn/add/add.php
+++ components/compare/btn/add/add.php
@@ -1,7 +1,9 @@
+<?php mgAddMeta('components/compare/btn/add/add.css') ?>
+
 <a href="<?php echo SITE . '/compare?inCompareProductId=' . $data["id"]; ?>"
    title="<?php echo lang('buttonCompare'); ?>"
    rel="nofollow"
-   class="js-add-to-compare addToCompare"
+   class="product-qty-group__btn compare-btn"
    data-item-id="<?php echo $data["id"]; ?> ">
     <?php echo lang('buttonCompare'); ?>
 </a>
