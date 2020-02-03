86d90c1de0d4306a10c024839a3c3bd4ff321011
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-03 13:17:24 +0300

    feat: стиль информера сравнения

--- components/compare/btn/add/add.php
+++ components/compare/btn/add/add.php
@@ -3,7 +3,7 @@
 <a href="<?php echo SITE . '/compare?inCompareProductId=' . $data["id"]; ?>"
    title="<?php echo lang('buttonCompare'); ?>"
    rel="nofollow"
-   class="product-qty-group__btn compare-btn"
+   class="product-qty-group__btn compare-btn js-add-to-compare"
    data-item-id="<?php echo $data["id"]; ?> ">
     <?php echo lang('buttonCompare'); ?>
 </a>
--- components/compare/informer/informer.php
+++ components/compare/informer/informer.php
@@ -2,9 +2,6 @@
 
 <div class="c-compare__informer js-compare-informer">
     <div class="c-compare__informer--content">
-        <svg class="icon icon--check">
-            <use xlink:href="#icon--check"></use>
-        </svg>
         <?php echo lang('compareAdded'); ?>
     </div>
 </div>
