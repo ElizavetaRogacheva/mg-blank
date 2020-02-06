e5a97271359aec84467a87feaca58a5fd60e6a9d
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-06 15:50:01 +0300

    feat: наполнение контактов и соцсетей в футере

---
+++ components/footer-menu/footer-menu.php
@@ -0,0 +1,8 @@
+
+<ul class="footer-info__list js-footer-info__list">
+    <li class="footer-info__item"><a title="" href="#" class="footer-info__link">About Us</a></li>
+    <li class="footer-info__item"><a title="" href="#" class="footer-info__link">Delivery Information</a></li>
+    <li class="footer-info__item"><a title="" href="#" class="footer-info__link">Privacy Policy</a></li>
+    <li class="footer-info__item"><a title="" href="#" class="footer-info__link">Terms & Conditions</a></li>
+    <li class="footer-info__item"><a title="" href="#" class="footer-info__link">Site Map</a></li>
+</ul>
--- layout/layout_footer.php
+++ layout/layout_footer.php
@@ -7,37 +7,55 @@
                     <div class="main-footer__block contacts">
                         <div class="main-footer__block-title" id="contact"><span><?php echo lang('contactUs')?></span></div>
                         <ul class="contacts__list js-contacts__list">
-                            <li class="contacts__item"><span>Cactuplan - Plant Store & United States</span></li>
-                            <li class="contacts__item"><span>000-000-0000</span></li>
-                            <li class="contacts__item"><span>123456</span></li>
-                            <li class="contacts__item"><span>sales@yourcompany.com</span></li>
+                            <li class="contacts__item"><span><?php echo MG::getSetting('shopName')?></span></li>
+                            <li class="contacts__item"><span><?php echo MG::getSetting('shopPhone')?></span></li>
+                            <li class="contacts__item"><span><?php echo MG::getSetting('shopAddress')?></span></li>
+                            <li class="contacts__item"><span><?php echo MG::getSetting('sitename')?></span></li>
                         </ul>
                     </div>
                     <div class="main-footer__block footer-info">
                         <div class="main-footer__block-title" id="info"><span><?php echo lang('footerMenu')?></span></div>
-                        <ul class="footer-info__list js-footer-info__list">
-                            <li class="footer-info__item"><a title="" href="#" class="footer-info__link">About Us</a></li>
-                            <li class="footer-info__item"><a title="" href="#" class="footer-info__link">Delivery Information</a></li>
-                            <li class="footer-info__item"><a title="" href="#" class="footer-info__link">Privacy Policy</a></li>
-                            <li class="footer-info__item"><a title="" href="#" class="footer-info__link">Terms & Conditions</a></li>
-                            <li class="footer-info__item"><a title="" href="#" class="footer-info__link">Site Map</a></li>
-                        </ul>
+                        <?php 
+                            component(
+                                'footer-menu',
+                                 $data
+                            ); 
+                        ?>
+
                     </div>     
+                    <?php if (MG::get('templateParams')['socialsShow'] == true) :?>
                     <div class="main-footer__block footer-newsletter">
                         <div class="main-footer__block-title main-footer__block-title--not-arrow"><span><?php echo lang('socials')?></span></div>
                         <div class="main-footer__socials">
                             <ul class="socials__list">
-                                <li class="socials__item"><a title="" href="#" class="social__link social__link--fb"></a></li>
-                                <li class="socials__item"><a title="" href="#" class="social__link social__link--tw"></a></li>
-                                <li class="socials__item"><a title="" href="#" class="social__link social__link--rss"></a></li>
-                                <li class="socials__item"><a title="" href="#" class="social__link social__link--google"></a></li>
-                                <li class="socials__item"><a title="" href="#" class="social__link social__link--ig"></a></li>
+                            <?php if (MG::get('templateParams')['footerFacebook'] == true) :?>
+                                <li class="socials__item">
+                                    <a title="" 
+                                       href="<?php echo (MG::get('templateParams')['footerFacebookUrl']) ?>" 
+                                       class="social__link social__link--fb"></a>
+                                </li>
+                            <?php endif ;?>
+                            <?php if (MG::get('templateParams')['footerInstagram'] == true) :?>
+                                <li class="socials__item">
+                                    <a title="" 
+                                       href="<?php echo (MG::get('templateParams')['footerInstagramUrl']) ?>" 
+                                       class="social__link social__link--ig"></a>
+                                </li>
+                            <?php endif ;?>
+                            <?php if (MG::get('templateParams')['footerVk'] == true) :?>
+                                <li class="socials__item">
+                                    <a title="" 
+                                       href="<?php echo (MG::get('templateParams')['footerVkUrl']) ?>" 
+                                       class="social__link social__link--vk"></a>
+                                </li>
+                            <?php endif ;?>
                             </ul>
                         </div>                       
-                    </div>           
+                    </div>  
+                    <?php endif ;?>         
                 </div>
                 <div class="main-footer-bottom">
-                    <div class="copyright"><span>Powered By OpenCart Cactuplan - Plant Store © 2020</span></div>
+                    <div class="copyright"><span><?php echo date('Y') . ' ' . lang('copyright'); ?></span></div>
                     <div class="main-footer-payment">
                         <ul class="payment__list">
                             <li class="payment__item"><img title="" src="<?php echo PATH_SITE_TEMPLATE ?>/img/visa.png" alt=""></li>
