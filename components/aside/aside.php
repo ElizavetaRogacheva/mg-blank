8a7eba36576dae36f685b2b3d19df523eff54248
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-11 16:42:06 +0300

    feat: рекламный блок в aside вынесен в config

--- components/aside/aside.php
+++ components/aside/aside.php
@@ -18,12 +18,18 @@
         );
         ?>
     </div>
-
-    <div class="advertising">
-        <a title="" href="#" class="advertising-block__link">
-            <img title="" src="./img/left-banner-1-278x320.jpg" alt="">
-        </a>
-    </div>
+    
+    <?php if (MG::get('templateParams')['asideBannerShow'] === '1') :?>
+        <div class="advertising">
+            <a title="<?php echo (MG::get('templateParams')['asideBannerLinkTitle']) ?>" 
+               href="<?php echo (MG::get('templateParams')['asideBannerLink']) ?>" 
+               class="advertising-block__link">
+                <img title="<?php echo (MG::get('templateParams')['asideBannerImgTitle']) ?>" 
+                     src="<?php echo PATH_SITE_TEMPLATE . MG::get('templateParams')['asideBannerImgUrl'] ?>" 
+                     alt="<?php echo (MG::get('templateParams')['asideBannerImgAlt']) ?>">
+            </a>
+        </div>
+    <?php endif ;?>
 
     <div class="latest-products">
     <?php if (class_exists('RecentlyViewed')) { ?>
@@ -35,52 +41,5 @@
             </div>
         <?php } ?>
     </div>
-    <!--<div class="latest-products">
-        <div class="latest-products__title js-latest-products__title aside-title"><span>latest</span></div>
-        <ul class="latest-products__list js-latest-products__list">
-            <li class="latest-products__item">
-                <div class="latest-product-card">
-                    <a title="" href="#" class="featured-product-card__link">
-                        <img title="" src="./img/05-360x302.jpg" alt="">
-                    </a>
-                    <div class="product-block-content">
-                        <h4 class="product-block__title latest-product-title">
-                            <a title="" href="#" class="latest-product-link">Consectetur Hampden</a>
-                        </h4>
-                        <p class="price latest-product-price">$110.00 <s>$119.60</s></p>
-                        <button class="latest-products__btn">add to cart</button>
-                    </div>
-                </div>
-            </li>
-            <li class="latest-products__item">
-                <div class="latest-product-card">
-                    <a title="" href="#" class="featured-product-card__link">
-                        <img title="" src="./img/03-360x302.jpg" alt="">
-                    </a>
-                    <div class="product-block-content">
-                        <h4 class="product-block__title latest-product-title">
-                            <a title="" href="#" class="latest-product-link">Aliquam Quaerat</a>
-                        </h4>
-                        <p class="price latest-product-price">$108.80</p>
-                        <button class="latest-products__btn">add to cart</button>
-                    </div>
-                </div>
-            </li>
-            <li class="latest-products__item">
-                <div class="latest-product-card">
-                    <a title="" href="#" class="featured-product-card__link">
-                        <img title="" src="./img/02-360x302.jpg" alt="">
-                    </a>
-                    <div class="product-block-content">
-                        <h4 class="product-block__title latest-product-title">
-                            <a title="" href="#" class="latest-product-link">Accusantium Doloremque</a>
-                        </h4>
-                        <p class="price latest-product-price">$104.00</p>
-                        <button class="latest-products__btn">add to cart</button>
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </div>-->
 </aside>
 
