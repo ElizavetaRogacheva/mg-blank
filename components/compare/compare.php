730805054b3d2c5f8acf46905550161976f7546d
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-17 16:57:28 +0300

    style: стилизация карточек товара на странице сравнения

--- components/compare/compare.php
+++ components/compare/compare.php
@@ -2,8 +2,8 @@
 <?php
 mgAddMeta('components/compare/compare.js');
 mgAddMeta('components/compare/compare.css');
-mgAddMeta('components/catalog/item/item.css');
-?>
+/* mgAddMeta('components/catalog/item/item.css');
+ */?>
 
 <?php
 if (!empty($data)) { ?>
@@ -56,8 +56,17 @@ if (!empty($data)) { ?>
                             <div class="c-goods__left">
                                 <a class="c-compare__remove mp-remove-compared-product"
                                    href="<?php echo SITE ?>/compare?delCompareProductId=<?php echo $item['id'] ?>">
-                                    <svg class="icon icon--close">
-                                        <use xlink:href="#icon--close"></use>
+                                   <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                            viewBox="0 0 512.001 512.001" style="enable-background:new 0 0 512.001 512.001;" xml:space="preserve">
+                                        <g>
+                                            <g>
+                                                <path d="M284.286,256.002L506.143,34.144c7.811-7.811,7.811-20.475,0-28.285c-7.811-7.81-20.475-7.811-28.285,0L256,227.717
+                                                    L34.143,5.859c-7.811-7.811-20.475-7.811-28.285,0c-7.81,7.811-7.811,20.475,0,28.285l221.857,221.857L5.858,477.859
+                                                    c-7.811,7.811-7.811,20.475,0,28.285c3.905,3.905,9.024,5.857,14.143,5.857c5.119,0,10.237-1.952,14.143-5.857L256,284.287
+                                                    l221.857,221.857c3.905,3.905,9.024,5.857,14.143,5.857s10.237-1.952,14.143-5.857c7.811-7.811,7.811-20.475,0-28.285
+                                                    L284.286,256.002z"/>
+                                            </g>
+                                        </g>
                                     </svg>
                                 </a>
                                 <a class="c-goods__img c-compare__img" href="<?php echo $item['link'] ?>">
