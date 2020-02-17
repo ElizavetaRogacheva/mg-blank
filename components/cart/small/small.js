6b5da042fb2a636f28e09ff439e1f6566eee438a
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-17 17:22:20 +0300

    style: увеличение ширины селекта на странице заказа

--- components/cart/small/small.js
+++ components/cart/small/small.js
@@ -3,9 +3,7 @@
 const headerCartBtn = document.querySelector('.mg-desktop-cart');
 const cartDropdownBlock = document.querySelector('.c-cart__dropdown');
 
-headerCartBtn.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    console.log('123');
+headerCartBtn.addEventListener('click', () => {
     if ($('.small-cart-table tr').length !== 0) {
         cartDropdownBlock.classList.toggle('small-cart-open');
     }
