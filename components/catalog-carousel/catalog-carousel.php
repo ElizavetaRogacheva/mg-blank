29823df45fc83444bb10e41f24218c21700a8e8e
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-19 16:10:15 +0300

    style: выравнивание карточек товара по центру на главной странице

--- components/catalog-carousel/catalog-carousel.php
+++ components/catalog-carousel/catalog-carousel.php
@@ -2,18 +2,20 @@
 <section class="popular-products-section">
     <div class="popular-products-block">
         <h2 class="popular-products__title italic-title"><?php echo $data['title']; ?></h2>
-        <ul class="popular-products__list">
-            <?php foreach($data[items] as $product) { ?>
-                <li class="popular-products__item">
-                <?php
-                component(
-                    'product-item',
-                    $product
-                );
-                ?>
-                </li>
-            <?php } ?>  
-        </ul>
+        <div class="popular-products__wrapper">
+            <ul class="popular-products__list">
+                <?php foreach($data[items] as $product) { ?>
+                    <li class="popular-products__item">
+                    <?php
+                    component(
+                        'product-item',
+                        $product
+                    );
+                    ?>
+                    </li>
+                <?php } ?>  
+            </ul>
+        </div>
         <a title="" href="<?php echo $data['link']; ?>" class="btn__link"><?php echo lang('viewMoreBtn')?></a>
     </div>
 </section>
