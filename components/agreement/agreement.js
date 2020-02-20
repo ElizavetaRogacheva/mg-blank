4ab5b483236b6b8b311c3deb95582e737076933b
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-20 16:47:18 +0300

    style: пустая строка в конце стилевых файлов

--- components/agreement/agreement.js
+++ components/agreement/agreement.js
@@ -1,37 +1,36 @@
-$(document).ready(function () {
+$(document).ready(function() {
     var modal = $('.js-agreement-modal');
     var modalOverlay = $('.js-modal-overlay');
     var btnOpenSelector = '.js-open-agreement';
     var btnCloseSelector = '.js-close-agreement';
 
-        // открытие модалки с соглашением на обработку пользовательских данных
-        $(document.body).on('click', btnOpenSelector, function (e) {
-            e.preventDefault();
+    // открытие модалки с соглашением на обработку пользовательских данных
+    $(document.body).on('click', btnOpenSelector, function(e) {
+        e.preventDefault();
 
-            if (modal.length < 1) {
-                $.ajax({
-                    type: "GET",
-                    url: mgBaseDir + "/ajaxrequest",
-                    data: {
-                        layoutAgreement: 'agreement'
-                    },
-                    dataType: "HTML",
-                    success: function (response) {
-                        $('body').append(response);
-                    }
-                });
-            } else {
-                modalOverlay.show();
-                modal.attr('open', true);
-            }
-        });
+        if (modal.length < 1) {
+            $.ajax({
+                type: 'GET',
+                url: mgBaseDir + '/ajaxrequest',
+                data: {
+                    layoutAgreement: 'agreement',
+                },
+                dataType: 'HTML',
+                success: function(response) {
+                    $('body').append(response);
+                },
+            });
+        } else {
+            modalOverlay.show();
+            modal.attr('open', true);
+        }
+    });
 
     // закрытие модалки с соглашением на обработку пользовательских данных
-    $(document.body).on('click', btnCloseSelector, function (e) {
+    $(document.body).on('click', btnCloseSelector, function(e) {
         e.preventDefault();
 
         modalOverlay.hide();
         modal.removeAttr('open');
     });
 });
-
