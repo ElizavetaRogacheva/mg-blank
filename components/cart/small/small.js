06e3559ffb34e29866598315cbb0530788d2fc30
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-20 16:37:12 +0300

    feat: открытие и проверка корзины на наполненность при наведении вместо клика

--- components/cart/small/small.js
+++ components/cart/small/small.js
@@ -3,30 +3,8 @@
 const headerCartBtn = document.querySelector('.mg-desktop-cart');
 const cartDropdownBlock = document.querySelector('.c-cart__dropdown');
 
-headerCartBtn.addEventListener('click', () => {
-    if ($('.small-cart-table tr').length !== 0) {
-        cartDropdownBlock.classList.toggle('small-cart-open');
-    }
-}); /* 
-document.addEventListener('click', function(evt) {
-    const target = evt.target;
-    const itsCart = target == cartDropdownBlock || cartDropdownBlock.contains(target);
-    const itsCartBtn = target == headerCartBtn;
-    const cartDropdownActive = cartDropdownBlock.classList.contains(
-        'small-cart-open'
-    );
-
-    if (!itsCart && !itsCartBtn && cartDropdownActive) {
-        if ($('.small-cart-table tr').length !== 0) {
-            cartDropdownBlock.classList.toggle('small-cart-open');
-        }
+headerCartBtn.addEventListener('mouseover', () => {
+    if ($('.small-cart-table tr').length === 0) {
+        cartDropdownBlock.style.display = 'none';
     }
 });
- */
-
-/* document.addEventListener('click', function(evt) {
-    if(!cartDropdownBlock.contains(evt.target) && cartDropdownBlock.classList.contains('small-cart-open')) {
-        cartDropdownBlock.classList.toggle('small-cart-open');
-    }
-});
- */
