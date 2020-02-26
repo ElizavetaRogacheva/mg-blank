64cd96369a4d8c390b4a8a350543cd69a8f43403
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-26 13:33:30 +0300

    feat: введена проверка на наличие фильтра для добавления обработчика события

--- components/aside/js/aside.js
+++ components/aside/js/aside.js
@@ -1,24 +1,11 @@
-'use strict'
+'use strict';
 
-const catTitle  = document.querySelector('.js-categories__title');
-const catList  = document.querySelector('.js-categories__list');
 const filterTitle = document.querySelector('.js-filter-title');
-const filterAsideList  = document.querySelector('.js-filter__list');
-const latestProd  = document.querySelector('.js-latest-products__title');
-const latestList  = document.querySelector('.js-latest-products__list');
+const filterAsideList = document.querySelector('.js-filter__list');
 
-catTitle.addEventListener('click', () => {
-    catList.classList.toggle('accordion-visible');
-    catTitle.classList.toggle('rotate');
-});
-
-filterTitle.addEventListener('click', () => {
-    filterAsideList.classList.toggle('accordion-visible');
-    filterTitle.classList.toggle('rotate');
-});
-
-latestProd.addEventListener('click', () => {
-    latestList.classList.toggle('accordion-visible');
-    latestProd.classList.toggle('rotate');
-});
-
+if (filterTitle) {
+    filterTitle.addEventListener('click', () => {
+        filterAsideList.classList.toggle('accordion-visible');
+        filterTitle.classList.toggle('rotate');
+    });
+}
