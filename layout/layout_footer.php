2701ec255784f1266a3ef9e6eadf18c34596c974
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-26 13:00:01 +0300

    feat: изменена функция вывода платежных систем в футер

--- layout/layout_footer.php
+++ layout/layout_footer.php
@@ -75,20 +75,49 @@
                     <div class="copyright"><span><?php echo date('Y') . ' ' . lang('copyright'); ?></span></div>
                     <?php if (MG::get('templateParams')['paymentShow'] === '1') :?>
                     <div class="main-footer-payment">
-                    <ul class="payment__list">
-                        <?php
-                        $res = DB::query(
-                            'SELECT name, id  FROM ' .
-                                PREFIX .
-                                'payment WHERE activity=1 ORDER BY id'
-                        );
-                        while ($payments = DB::fetchAssoc($res)) { ?>
-                                <li title="<?php echo $payments['name']; ?>"
-                                    class="payment__item icon-style <?php echo 'pay-' .
-                                    $payments['id']; ?>"><?php echo $payments['name']; ?></li>
-                            <?php }
-                        ?>
-                    </ul>
+                       <?php
+                            $paymentIdToIconName = [
+                            '1' => 'webmoney.png',
+                            '2' => 'ya.png',
+                            '12' => 'ya.png',
+                            '5' => 'robo.png',
+                            '6' => 'qiwi.png',
+                            '8' => 'sci.png',
+                            '9' => 'payanyway.png',
+                            '10' => 'paymaster.png',
+                            '11' => 'alfabank.png',
+                            '14' => 'yandexkassa.png',
+                            '15' => 'privat24.png',
+                            '16' => 'liqpay.png',
+                            '17' => 'sber.png',
+                            '18' => 'tinkoff.png',
+                            '19' => 'paypal.png',
+                            '21' => 'paykeeper.png',
+                            '20' => 'comepay.svg',
+                            '22' => 'cloudpayments.png',
+                            '23' => 'ya-pay-parts.svg',
+                            '24' => 'yandexkassa.png',
+                            '25' => 'apple.png',
+                            '26' => 'free-kassa.png',
+                            '27' => 'megakassa.png',
+                            '28' => 'qiwi.png',
+                            ];
+
+                            $res = DB::query(
+                            'SELECT name, id FROM ' .
+                            PREFIX .
+                            'payment WHERE activity=1 ORDER BY id'
+                            );
+
+                            while ($payments = DB::fetchAssoc($res)) {
+                            $imgName = isset($paymentIdToIconName[$payments['id']]) ? $paymentIdToIconName[$payments['id']] : 'cash.png';
+                            ?>
+                            <li title="Доступен способ оплаты «<?php echo $payments['name']; ?>»"
+                            class="payment__item">
+                            <img src="<?php echo SITE . '/mg-admin/design/images/icons/' . $imgName; ?>"
+                            alt="Доступен способ оплаты «<?php echo $payments['name']; ?>»">
+                            </li>
+                            <?php } ?>
                     </div>
                     <?php endif ; ?>
                 </div>
