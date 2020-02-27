931cfadaabf3fbe6794ed0289ac4e48a3eb1f572
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2020-02-27 10:45:41 +0300

    style: стилизация дополнительных полей на странице продукта

--- components/product/opfields/opfields.php
+++ components/product/opfields/opfields.php
@@ -1,15 +1,16 @@
+<?php mgAddMeta('components/opfields/opfields.css') ;?>
 <?php
 if (EDITION == 'gipermarket') {
     $opFieldsM = new Models_OpFieldsProduct($data['id']);
     $fields = $opFieldsM->get();
 
     foreach ($fields as $key => $value) {
+        console_log($value['name']) ;
         if ($value['active'] == 0 || (empty($value['value']) && empty($value['variant']) )) continue;
-        echo 'asdasd';
         if ($data['variant']) { ?>
             <div class="product-opfield">
                 <span class="product-opfield__name">
-                    <?php echo $value['name'] ?>
+                    <?php echo $value['name'] ?>:
                 </span>
                 <span class="product-opfield__value">
                     <?php echo $value['variant'][$data['variant']]['value'] ?>
